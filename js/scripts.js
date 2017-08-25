7aaebddc5c5edf58dd09d07f99352586ccfeb8e6
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-08-25 13:03:17 -0700

    add css element to hide table, rename var topping

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-function Pizza(topping, topping2, topping3, size, price) {
-  this.topping = topping;
+function Pizza(topping1, topping2, topping3, size, price) {
+  this.topping1 = topping1;
   this.topping2 = topping2;
   this.topping3 = topping3;
   this.size = size;
@@ -11,11 +11,11 @@ function formula(pizza) {
     pizza.price -= 2;
   } else if(pizza.size === "Large") {
     pizza.price += 2;
-  }else if(pizza.topping === "Anchovy") {
+  } if(pizza.topping1 === "Anchovy") {
     pizza.price += 1.5;
-  } else if(pizza.topping === "Sausage") {
+  } else if(pizza.topping1 === "Sausage") {
     pizza.price += 1.5;
-} else if(pizza.topping === "Mushroom") {
+} else if(pizza.topping1 === "Mushroom") {
   pizza.price += 1.5;}
 }
 
@@ -23,16 +23,16 @@ $(document).ready(function(){
   $("#submit").click(function(event){
     event.preventDefault();
 
-    var topping = $("#topping option:selected").text();
+    var topping1 = $("#topping1 option:selected").text();
     var topping2 = $("#topping2 option:selected").text();
     var topping3 = $("#topping3 option:selected").text();
     var size = $("#size option:selected").text();
     var price = 15;
-    var buyPizza = new Pizza(topping, topping2, topping3, size, price);
+    var buyPizza = new Pizza(topping1, topping2, topping3, size, price);
 
     price = formula(buyPizza);
 
-    $("#toppingOut").text(buyPizza.topping);
+    $("#topping1Out").text(buyPizza.topping1);
     $("#topping2Out").text(buyPizza.topping2);
     $("#topping3Out").text(buyPizza.topping3);
     $("#sizeOut").text(buyPizza.size);
