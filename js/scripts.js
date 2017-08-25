922e7ee06d12c2f5521bcc082009a91636148971
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-08-25 10:33:01 -0700

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,26 @@ function formula(pizza) {
     pizza.price += 2;
   }
 }
+
+$(document).ready(function(){
+  $("#submit").click(function(event){
+    event.preventDefault();
+
+    var topping = $("#topping option:selected").text();
+    var topping2 = $("#topping2 option:selected").text();
+    var topping3 = $("#topping3 option:selected").text();
+    var size = $("#size option:selected").text();
+    var price = 15;
+    var buyPizza = new Pizza(topping, topping2, topping3, size, price);
+
+    price = formula(buyPizza);
+
+    $("#toppingOut").text(buyPizza.topping);
+    $("#topping2Out").text(buyPizza.topping2);
+    $("#topping3Out").text(buyPizza.topping3);
+    $("#sizeOut").text(buyPizza.size);
+    $("#priceOut").text(buyPizza.price);
+
+    $(".table").show()
+  });
+});
